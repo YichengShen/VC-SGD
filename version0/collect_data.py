093e917dd50c89931dede657593fff26d78a507d
YichengShen <jsszsyc@hotmail.com>
2021-05-18 23:16:13 -0400

    zone75 vc0 vc3

---
+++ version0/collect_data.py
@@ -0,0 +1,17 @@
+import ruamel.yaml
+import yaml
+import os
+
+
+
+for num_vehicular_clouds in [0, 3]:
+    for I in range(1,6):
+        with open('config.yml', 'r') as file:
+            cfg = yaml.load(file, Loader=yaml.FullLoader)
+            cfg['num_vc'] = num_vehicular_clouds
+            cfg['simulation']['num_vc'] = num_vehicular_clouds
+
+        with open('config.yml', 'w') as fp:
+            yaml.dump(cfg, fp)
+
+        os.system(f"python3 main.py --num-round {I}")
